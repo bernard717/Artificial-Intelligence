18424f65542708ddc2ee4b130a7c79f1c405a9d9
bernard717 <41356074+bernard717@users.noreply.github.com>
2019-11-17 15:48:25 +0900

    Create connect4_game.h

---
+++ connect4_game.h
@@ -0,0 +1,20 @@
+#pragma once
+extern int board[10][10], len[10], turn, dx[4], dy[4];
+
+void print_board();
+
+void put_piece(int, bool);
+
+void deput_piece(int);
+
+int putable_both_side(int);
+
+bool double_three(int);
+
+int get_max_length_connected(int);
+
+int get_unlimit_length_connected(int, int);
+
+int get_overflow_length_connected(int, int);
+
+int get_current_length_connected(int, int);
